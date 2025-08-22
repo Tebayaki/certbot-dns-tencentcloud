e4c2c4da4c7297a2e9147fb3ed7ddda92811a56a
Tebayaki <kongjiancong@qq.com>
2025-08-22 22:00:38 +0800

    fix: typo

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-if [ -n "$PUID" ] && [ -n "$GUID" ]; then
-    echo "Running with PUID=${PUID} and GUID=${GUID}"
+if [ -n "$PUID" ] && [ -n "$PGID" ]; then
+    echo "Running with PUID=${PUID} and PGID=${PGID}"
 
     if [ "$(id -u user1)" != "$PUID" ]; then
         echo "Modifying user1 UID from $(id -u user1) to $PUID"
         usermod -o -u "$PUID" user1
     fi
 
-    if [ "$(id -g user1)" != "$GUID" ]; then
-        echo "Modifying user1 GID from $(id -g user1) to $GUID"
-        groupmod -o -g "$GUID" user1
+    if [ "$(id -g user1)" != "$PGID" ]; then
+        echo "Modifying user1 GID from $(id -g user1) to $PGID"
+        groupmod -o -g "$PGID" user1
     fi
 
     chown -R user1:user1 /app /data
